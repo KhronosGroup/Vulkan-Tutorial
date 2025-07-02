7e5cd04b979bc50539967e63530113934cafd04b
asuessenbach <asuessenbach@nvidia.com>
2025-07-02 17:41:40 +0200

    Fix Instance creation in 01_instance_creation.cpp

--- attachments/01_instance_creation.cpp
+++ attachments/01_instance_creation.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
@@ -58,11 +59,27 @@ private:
             .pEngineName        = "No Engine",
             .engineVersion      = VK_MAKE_VERSION( 1, 0, 0 ),
             .apiVersion         = vk::ApiVersion14 };
+
+        // Get the required instance extensions from GLFW.
         uint32_t glfwExtensionCount = 0;
         auto glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        // Check if the required GLFW extensions are supported by the Vulkan implementation.
+        auto extensionProperties = context.enumerateInstanceExtensionProperties();
+        for (uint32_t i = 0; i < glfwExtensionCount; ++i)
+        {
+            if (std::ranges::none_of(extensionProperties,
+                                     [glfwExtension = glfwExtensions[i]](auto const& extensionProperty)
+                                     { return strcmp(extensionProperty.extensionName, glfwExtension) == 0; }))
+            {
+                throw std::runtime_error("Required GLFW extension not supported: " + std::string(glfwExtensions[i]));
+            }
+        }
+
         vk::InstanceCreateInfo createInfo{
             .pApplicationInfo = &appInfo,
-            .ppEnabledLayerNames = glfwExtensions};
+            .enabledExtensionCount = glfwExtensionCount,
+            .ppEnabledExtensionNames = glfwExtensions};
         instance = vk::raii::Instance(context, createInfo);
     }
 };

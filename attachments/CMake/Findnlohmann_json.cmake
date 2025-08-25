e1ac508ec5a19ebffbc062c2e4f536c23a5d0359
swinston <steve@holochip.com>
2025-08-25 02:14:27 -0700

    updating nlohmann_json to v3.12.0.

--- attachments/CMake/Findnlohmann_json.cmake
+++ attachments/CMake/Findnlohmann_json.cmake
@@ -47,7 +47,7 @@ if(NOT nlohmann_json_INCLUDE_DIR)
   FetchContent_Declare(
     nlohmann_json
     GIT_REPOSITORY https://github.com/nlohmann/json.git
-    GIT_TAG v3.11.2  # Use a specific tag for stability
+    GIT_TAG v3.12.0  # Use a specific tag for stability
   )
 
   # Set policy to suppress the deprecation warning

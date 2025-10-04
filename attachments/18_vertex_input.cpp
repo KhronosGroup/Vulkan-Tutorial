33283db0b82afce2c22c01946ce38306d1ec9e8d
Sascha Willems <webmaster@saschawillems.de>
2025-10-04 12:30:32 +0200

    Use reference in loop

--- attachments/18_vertex_input.cpp
+++ attachments/18_vertex_input.cpp
@@ -344,7 +344,7 @@ private:
 
         vk::ImageViewCreateInfo imageViewCreateInfo{ .viewType = vk::ImageViewType::e2D, .format = swapChainSurfaceFormat.format,
           .subresourceRange = { vk::ImageAspectFlagBits::eColor, 0, 1, 0, 1 } };
-        for ( auto image : swapChainImages )
+        for (auto& image : swapChainImages)
         {
             imageViewCreateInfo.image = image;
             swapChainImageViews.emplace_back( device, imageViewCreateInfo );

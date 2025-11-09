9aee1c5d875683d89d4329382c58b3d3ca0f86b1
gavrix32 <superdimoxa228@gmail.com>
2025-11-09 22:30:07 +0300

    Update queue priority to 0.5f as requested

--- attachments/31_compute_shader.cpp
+++ attachments/31_compute_shader.cpp
@@ -370,7 +370,7 @@ class ComputeShaderApplication
 		    };
 
 		// create a Device
-		float                     queuePriority = 0.0f;
+		float                     queuePriority = 0.5f;
 		vk::DeviceQueueCreateInfo deviceQueueCreateInfo{.queueFamilyIndex = queueIndex, .queueCount = 1, .pQueuePriorities = &queuePriority};
 		vk::DeviceCreateInfo      deviceCreateInfo{.pNext                   = &featureChain.get<vk::PhysicalDeviceFeatures2>(),
 		                                           .queueCreateInfoCount    = 1,
